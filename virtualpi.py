94f79f0c3442617d9514efba42622fc83dfad52c
davidbrodrick <david.brodrick@anu.edu.au>
2023-05-01 15:35:47 +1000

    Generalise code further.

--- virtualpi.py
+++ virtualpi.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 
-#The following environment variables need to be set:
-#
-#
+#NB: The following environment variables need to be set:
+# export OPENAI_API_KEY="sk-M...M"
+# export SLACK_APP_TOKEN="xapp-1...d"
+# export SLACK_BOT_TOKEN="xoxb-2...C"
+#See the README for more information: 
 
-#Imports - pip3 install paper
-import sys, os, pickle
+import sys, os, pickle, glob
 from paperqa import Docs
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+#Create Slack app handle
+app = App(token=os.environ["SLACK_BOT_TOKEN"])
 
 ############################################################
 #This function is called when a Slack user mentions the bot
@@ -35,22 +39,45 @@ def event_test(say, body):
 
 #We require a directory containing PDFs as an argument
 if len(sys.argv)!=2:
-    raise ValueError("Requires the path to a repository of PDFs as an argument.")
+    print("Requires the path to a repository of PDFs as an argument.")
+    sys.exit(1)
 PAPERDIR=sys.argv[1]
+if not os.path.exists(PAPERDIR):
+    print("The specified directory does not exist.")
+    sys.exit(1)
 
 try:
-    # load
-    with open("my_docs.pkl", "rb") as f:
-        docs = pickle.load(f)    
+    #Load the pre-pickled document vector if it exists
+    with open("%s/docs.pkl"%PAPERDIR, "rb") as f:
+        docs = pickle.load(f)
+    print("Loaded previous state from %s/docs.pkl"%PAPERDIR)
+    print(" - remove this file if you change the set of PDFs\n")
 except:
-    #save    
+    #Couldn't load a pre-picked version
+    papers=[]
+    filesfound=glob.glob("%s/*"%PAPERDIR)
+    for p in filesfound:
+        if p.lower().endswith(".pdf"):
+            papers.append(p)
+
+    if not papers:
+        print("No PDFs were found in the specified directory.")
+        sys.exit(1)
+
+    print("Found %d PDFs in %s"%(len(papers),PAPERDIR))
+
+    #Add each paper in turn to paper-qa/FAISS     
     docs = Docs(llm='gpt-3.5-turbo', summary_llm="davinci")
     for p in papers:
-        docs.add(PAPERDIR+p, citation=p, key=p) #, chunk_chars=2500)
-    with open("my_docs.pkl", "wb") as f:
-        pickle.dump(docs, f)
+        print("Embedding %s .."%p)
+        docs.add(p, citation=p, key=p)
+    try:
+        with open("%s/docs.pkl"%PAPERDIR, "wb") as f:
+            #Save this state for next time
+            pickle.dump(docs, f)
+    except:
+        Print("Couldn't save state into %s - is it writeable?"%PAPERDIR)
 
-# Install the Slack app and get xoxb- token in advance
-app = App(token=os.environ["SLACK_BOT_TOKEN"])
-
+#Set up the Slack interface to start servicing requests
+print("Starting Slack handler - bot is ready to answer your questions!")
 SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
